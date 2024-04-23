0d8175b7b5233a3388115e68e047aee58abf77b0
supersaif08 <saifali30831@gmail.com>
2024-04-23 23:02:05 +0530

    variable's

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // data types
 // bollean values
-let my_name=true;
+/*let my_name=true;
 console.log(my_name);
 
 // typeof
@@ -23,3 +23,21 @@ console.log(typeof name);
 
 name="saif";//given the value to undefined
 console.log(typeof name);//string now!!
+
+// variable's 
+// three types [ let, var, const]
+// let veriable can be changed later
+let age=31;//first assigned 31
+age=40;//then changed to 40
+
+//constant variable can not be changed once it is assigned
+const birthyear=2003;//once assigned can't be changed
+birthyear=2004;//error
+const job;//constant variable can't be empty
+
+// var variable
+// var is same as let variable but not recommended to use
+var first_name="md";
+first_name="saif";
+console.log(first_name)*/
+
