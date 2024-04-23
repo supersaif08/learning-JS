21c5792c8c82e34be9de1e2a12b283c9d28a25d8
supersaif08 <saifali30831@gmail.com>
2024-04-23 22:47:26 +0530

    data_types

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("hello this is my first line in js code!!")
+// console.log("hello this is my first line in js code!!")
+
+// data types
+// bollean values
+let my_name=true;
+console.log(my_name);
+
+// typeof
+console.log(typeof true);//boolean
+console.log(typeof "hello");//string
+console.log(typeof my_name);//boolean
+console.log(typeof 123);//number
+console.log(typeof saif_ali);//undefined
+
+my_name="saif_ali";//changed the data-type to striing
+console.log(my_name);
+console.log(typeof my_name);//string
+
+
+let name;//undefined
+console.log(name);
+console.log(typeof name);
+
+name="saif";//given the value to undefined
+console.log(typeof name);//string now!!
